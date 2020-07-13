deba7ba2bcd12df41521f1853436f71d8a0cecf7
junlong321 <desmondwongjunlong@yahoo.com.sg>
2020-07-13 18:41:28 +0800

    Add STATIC_ROOT in an attempt to resolve deployment bug

--- djangomain/djangomain/settings.py
+++ djangomain/djangomain/settings.py
@@ -149,12 +149,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/images/'
 
-# Static files for ReactJS
+# Static files for ReactJS and Django
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    STATIC_ROOT,
     STATIC_DIR,
 ]
 
