e5ec0436609063e981fa828ce90670fbc30e764d
junlong321 <desmondwongjunlong@yahoo.com.sg>
2020-07-15 17:50:46 +0800

    Change host 8000 to pythonanywhere for frontend

--- djangomain/reactjs/src/components/axios/axiosDb.js
+++ djangomain/reactjs/src/components/axios/axiosDb.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosDb = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://junlong321.pythonanywhere.com',
 });
 
 axiosDb.defaults.headers.common['Authorization'] =
